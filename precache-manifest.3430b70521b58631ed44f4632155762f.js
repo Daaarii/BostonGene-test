56353f64a5497a2df5ffdef37b89a1c1188b18eb
Daaarii <zuganich48@yandex.ru>
2020-09-10 23:02:21 +0300

    Updates

---
+++ precache-manifest.3430b70521b58631ed44f4632155762f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503b63395c17d8498cbc9ce8a0cc6d8c",
+    "url": "/BostonGene-test/index.html"
+  },
+  {
+    "revision": "b11dce08cd0cb6a902aa",
+    "url": "/BostonGene-test/static/css/main.f88d77c0.chunk.css"
+  },
+  {
+    "revision": "63769bd3f513bd424535",
+    "url": "/BostonGene-test/static/js/2.430f1c09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BostonGene-test/static/js/2.430f1c09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b11dce08cd0cb6a902aa",
+    "url": "/BostonGene-test/static/js/main.ffc2d991.chunk.js"
+  },
+  {
+    "revision": "7dafdae166789c79681a",
+    "url": "/BostonGene-test/static/js/runtime-main.0dc792d7.js"
+  }
+]);
